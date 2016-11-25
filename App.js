4980427d5ed4c5f61cd44776d5c55309a33eb687
anahenneberke <ahenneberke@ahennebe-jg0904.justgiving.local>
2016-11-25 16:53:40 +0000

    Lesson 6 Update

--- App.js
+++ App.js
@@ -5,16 +5,14 @@ class App extends React.Component {
 	constructor(){
 		super();
 		this.state = { txt: ''}
-		this.update = this.update.bind(this)
+		this.update = this.update.bind(this) //???
 	}
 	update(e){
 		this.setState({txt: e.target.value})
 	}
 	render() {
-		// let txt = this.props.txt
 		return (
 			<div>
-				<Widget txt={this.state.txt} update={this.update} />
 				<Widget txt={this.state.txt} update={this.update} />
 			</div>
 		);
@@ -27,25 +25,12 @@ return (
 			<input type="text"
 				onChange={props.update}/>
 			<h1>{props.txt}</h1>
-			</div>
+		</div>
 		);	
 }
-
-// App.propTypes = {
-// 	txt: React.PropTypes.string,
-// 	cat: React.PropTypes.number.isRequired
-// }
-
-// App.defaultProps = {
-// 	txt: 'this is the default txt'
-// }
 
 ReactDOM.render(
 	<App cat={5} />,
 	document.getElementById('app')
 );
 
-// stateless function component
-// const App = () => <h1>Hello world!</h1>
-
-// export default App
